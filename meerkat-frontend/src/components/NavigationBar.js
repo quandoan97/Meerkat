c3204da60b03ad1ba0613a37b5fc2167a270ac46
sauravsubedi25 <49078146+sauravsubedi25@users.noreply.github.com>
2020-05-03 15:06:49 -0500

    create room form added and nav changes

--- meerkat-frontend/src/components/NavigationBar.js
+++ meerkat-frontend/src/components/NavigationBar.js
@@ -7,26 +7,27 @@ import '../App.css'
 export default class NavigationBar extends Component {
     render() {
         return (
-            
+
             <nav class="navbar navbar-expand-lg navbar-static-top">
                 <div>
-                    <Navbar.Brand > 
+                    <Navbar.Brand >
                         <Link to="/"><img src="https://mdbootstrap.com/img/logo/mdb-transparent.png" height="30" alt="mdb logo"></img></Link>
                     </Navbar.Brand>
-                </div>
-                <div >
-                    
                     <ul class="nav navbar-nav navbar-right">
                     <ul class="nav nav-pills">
                     <li class="nav-item"><a href="/Register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
                     <li class="nav-item"><a href="/Login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    
                     </ul>
                     </ul>
+                    <div id="mySidenav" class="sidenav">
+                        <a href="/userpage" id="profile">Profile</a>
+                        <a href="/create" id="create">Create Room</a>
+                        <a href="/View" id="view">View Rooms</a>
+                        <a href="#" id="about">About Us</a>
+                      </div>
                 </div>
-                
+
             </nav>
         )
     }
 }
-
