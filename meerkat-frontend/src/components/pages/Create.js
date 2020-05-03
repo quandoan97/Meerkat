4c7282343f80a3ecee63ff6d4526a75f3d9eab01
sauravsubedi25 <49078146+sauravsubedi25@users.noreply.github.com>
2020-05-03 15:04:20 -0500

    create room form added and nav changes

--- meerkat-frontend/src/components/pages/Create.js
+++ meerkat-frontend/src/components/pages/Create.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { TextField, Button,Input } from '@material-ui/core/'
+import { useForm } from 'react-hook-form';
+
 
 export default function Create() {
+      const {register, handleSubmit, errors} = useForm();
+
+      const onSubmit = (data) => {
+          console.log(data);
+      };
+
         return (
             <div>
                 <h1> Room Creation </h1>
+                <form onSubmit={ handleSubmit(onSubmit) }>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <input type="text" name="roomName" placeholder="Name Your Room" ref={ register({ required: "Please enter room name", minLength: {value: 2, message: "must be at least 2 characters"} }) }/>
+                                {errors.roomName && <p> { errors.roomName.message } </p>}
+                            </div>
+                            <div className="col">
+                                <input type="text" name="uniquePin" placeholder="Room Pin Number" ref={ register({ required: true, minLength: {value: 2, message: "Must be at least 2 characters"} }) }/>
+                                {errors.uniquePin && <p> { errors.uniquePin.message } </p>}
+                            </div>
+                            <div className="col">
+                                <input type="text" name="genre" placeholder="genre" ref={ register }/>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <Button variant="contained" onClick={ handleSubmit(onSubmit) }> Submit </Button>
+                    </div>
+                </form>
             </div>
         )
 }
--- meerkat-frontend/src/components/pages/Home.js
+++ meerkat-frontend/src/components/pages/Home.js
@@ -8,17 +8,16 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <div id="mySidenav" class="sidenav">
-                    <a href="/userpage" id="profile">Profile</a>
-                    <a href="/create" id="create">Create Room</a>
-                    <a href="/View" id="view">View Rooms</a>
-                    <a href="#" id="about">About Us</a>
-                </div>
+                  <div id="myhomecenter">
+                  <h1> Meerkat <Link to="/"><img src="https://images.squarespace-cdn.com/content/v1/5344b511e4b0f9ffdd2f4841/1521017739604-ZLMWFJK956X1HJF6OPOL/ke17ZwdGBToddI8pDm48kFgn1pslFD_UBE0eFFMtvglZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHxphHZ44Pm_4vAF5TeJm73sFgJc9vxs1qDpNyVTVrPpTqWIIaSPh2v08GbKqpiV54/The+GreatZoo+Escape+2%3A+Meerkat+-+Run+Animation"
+                  height = "50" alt="meerkatgif"></img></Link></h1>
+                  <h2> A new way to enjoy videos together and communicate with your friends and family</h2>
+                  </div>
+
                 <footer class="container-fluid text-center">
                     <p>All rights reserved || Meerkat</p>
                 </footer>
-            </div>
+                </div>
         )
     }
 }
-
